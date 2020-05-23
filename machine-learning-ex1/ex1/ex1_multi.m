3d731a909c710fa6fc6c23c4c5b07c1b56b8dd9a
Ozan Ürkmez <arkantos250@gmail.com>
2020-05-23 11:04:09 +0300

    remove visualization as it doesnt make sense with 4 variables

--- machine-learning-ex1/ex1/ex1_multi.m
+++ machine-learning-ex1/ex1/ex1_multi.m
@@ -115,41 +115,6 @@ fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-%% ============= Part 2.5: Visualizing J(theta_0, theta_1) =============
-fprintf('Visualizing J(theta_0, theta_1) ...\n')
-
-% Grid over which we will calculate J
-theta0_vals = linspace(-10, 10, 100);
-theta1_vals = linspace(-1, 4, 100);
-
-% initialize J_vals to a matrix of 0's
-J_vals = zeros(length(theta0_vals), length(theta1_vals));
-
-% Fill out J_vals
-for i = 1:length(theta0_vals)
-    for j = 1:length(theta1_vals)
-	  t = [theta0_vals(i); theta1_vals(j)];
-	  J_vals(i,j) = computeCost(X, y, t);
-    end
-end
-
-
-% Because of the way meshgrids work in the surf command, we need to
-% transpose J_vals before calling surf, or else the axes will be flipped
-J_vals = J_vals';
-% Surface plot
-figure;
-surf(theta0_vals, theta1_vals, J_vals)
-xlabel('\theta_0'); ylabel('\theta_1');
-
-% Contour plot
-figure;
-% Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
-xlabel('\theta_0'); ylabel('\theta_1');
-hold on;
-plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
-
 %% ================ Part 3: Normal Equations ================
 
 fprintf('Solving with normal equations...\n');
@@ -184,7 +149,7 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+price = [1, 1650, 3] * theta; % You should change this
 
 
 % ============================================================
