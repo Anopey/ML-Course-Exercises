e4cd6bd2e2d29c8b9e8ae7d25efe6e51b1a70f06
Ozan Ürkmez <arkantos250@gmail.com>
2020-05-27 13:55:08 +0300

    implment plot data

--- machine-learning-ex2/ex2/plotData.m
+++ machine-learning-ex2/ex2/plotData.m
@@ -13,9 +13,10 @@ figure; hold on;
 %
 
 
+pos = find(y==1); neg = find(y == 0); %find returns the indices of all values that are not equal to 0
 
-
-
+plot(X(pos, 1), X(pos, 2), 'k+','LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
 
 
 
