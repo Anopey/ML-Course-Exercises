c703bd5e5ed7c9dc1681b135264844af5fb1460c
Ozan Ürkmez <arkantos250@gmail.com>
2020-05-27 23:11:31 +0300

    implement cost function

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,8 +20,10 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+sigmo = sigmoid(X * theta)
+J = (-1/m) * ((y' * log(sigmo)) + ((1 - y)' * log(1-sigmo)))
 
-grad = 1/m * (X' * (sigmoid(X * theta) - y))
+grad = 1/m * (X' * (sigmo - y));
 
 
 
